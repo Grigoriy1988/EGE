8ef19d0fd2f13c18c5d42a920ed5d185788f0060
Prepod-i2 <bg_1988@mail.ru>
2024-10-01 10:26:06 +1200

    Задание 5

---
+++ 05 задание/06 задача.py
@@ -0,0 +1,21 @@
+цифры = '0123456789ABCDEF'
+
+
+def перевод(a, new):
+    dig_s = ''
+    while a > 0:
+        dig_s = цифры[a % new] + dig_s
+        a = a // new
+    return dig_s
+
+
+for n in range(1, 2000):
+    n_3 = перевод(n, 3)
+    if n % 3 == 0:
+        n_3 = "1"+n_3 +"02"
+    else:
+        n_3 = n_3 + перевод(n%3*4,3)
+    R = int(n_3,3)
+    if R <199:
+        print(f'N={n} троичное {n_3} R = {R}')
+
