6096aee5bfc324e849017b425ec102fd2da19954
Prepod-i2 <bg_1988@mail.ru>
2024-10-01 09:46:55 +1200

    Задание 5

--- 05 задание/03 задача.py
+++ 05 задание/03 задача.py
@@ -1,7 +1,7 @@
 for n in range(1,50):
     bin_n = bin(n)[2:]
     bin_n = bin_n + bin_n[-1]
-    if bin_n.count('1') % 2 == 0:
+    if bin(n)[2:].count('1') % 2 == 0:
         bin_n = bin_n + '0'
     else:
         bin_n = bin_n + '1'
@@ -11,10 +11,5 @@ for n in range(1,50):
     else:
         bin_n = bin_n + '1'
     R = int(bin_n,2)
-    if R>97:
+    if R >90:
         print(f"N = {n}, bin_r = {bin_n}, R = {R}")
-        break
-
-
-
-    # print(n,bin_n)
