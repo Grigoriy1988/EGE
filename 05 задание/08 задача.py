9d1612269082d67171bf614c6c185b60dfd4ef08
Grigoriy <bg_1988@mail.ru>
2024-10-01 17:07:56 +1200

    ege

--- 05 задание/08 задача.py
+++ 05 задание/08 задача.py
@@ -1,6 +1,4 @@
 цифры = '0123456789ABCDEFGHI' #19 cc
-
-
 def перевод(a, new):
     dig_s = ''
     while a > 0:
@@ -10,25 +8,26 @@ def перевод(a, new):
 r_max = 0
 for n in range(10000, 100000):
     n_19 = перевод(n, 19)
-    # print(n_19, end=' ')
+    # print(f"n={n} n_19={n_19}")
     # согласные буквы (В, C, D, F, G, H) заменяются на 5;
     sogl = 'BCDFGH'
     for i in sogl:
         n_19 = n_19.replace(i,'5')
-    # print(n_19,end=' ')
-    n_19 = перевод(n%19,19)+n_19
-    # print(n_19, end=' ')
+    # print(f"n={n} n_19={n_19}")
+    n_19 = перевод(n%19,19)+n_19 if n % 19 !=0 else n_19
+    # print(f"n={n} n_19={n_19}")
     n_19 = n_19[-2:] + n_19[:-2]
-    # print(n_19)
+
+    # print(f"n={n} n_19={n_19}")
 
     #повтор
     for i in sogl:
-        n_19 = n_19.replace(i,'5')
-    # print(n_19,end=' ')
-    # n_19 = перевод(n%19,19)+n_19 спорный момент
-    # print(n_19, end=' ')
+        n_19 = n_19.replace(i, '5')
+    # print(f"n={n} n_19={n_19}")
+    n_19 = перевод(n % 19, 19) + n_19 if n % 19 != 0 else n_19
+    # print(f"n={n} n_19={n_19}")
     n_19 = n_19[-2:] + n_19[:-2]
-    # print(n_19)
+    # print(f"n={n} n_19={n_19}")
     r= int(n_19,19)
     s = sum(map(int,str(r)))
     if s%7 ==0:
