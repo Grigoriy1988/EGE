a40a4bc9a7e5981860615ab2e4dcecf5305a0ce6
Prepod-i2 <bg_1988@mail.ru>
2024-10-01 13:28:59 +1200

    Задание 5

--- 05 задание/01 Задача.py
+++ 05 задание/01 Задача.py
@@ -1,16 +1,16 @@
 for n in range(1, 50):
-    bin_n =bin(n)[2:]
-    if bin_n.count('1') %2 ==0:
+    bin_n = bin(n)[2:]
+    if bin_n.count('1') % 2 == 0:
         bin_n = bin_n + '0'
     else:
         bin_n = bin_n + '1'
 
-    if bin_n.count('1') %2 ==0:
+    if bin_n.count('1') % 2 == 0:
         bin_n = bin_n + '0'
     else:
         bin_n = bin_n + '1'
-    R = int(bin_n,2)
-    #print(n, bin_n,R)
-    if R>130:
-        print(n, bin_n,R)
-        #break
+    R = int(bin_n, 2)
+    # print(n, bin_n,R)
+    if R > 130:
+        print(n, bin_n, R)
+        # break
--- 05 задание/03 задача.py
+++ 05 задание/03 задача.py
@@ -1,4 +1,4 @@
-for n in range(1,50):
+for n in range(1, 50):
     bin_n = bin(n)[2:]
     bin_n = bin_n + bin_n[-1]
     if bin(n)[2:].count('1') % 2 == 0:
@@ -10,6 +10,6 @@ for n in range(1,50):
         bin_n = bin_n + '0'
     else:
         bin_n = bin_n + '1'
-    R = int(bin_n,2)
-    if R >90:
+    R = int(bin_n, 2)
+    if R > 90:
         print(f"N = {n}, bin_r = {bin_n}, R = {R}")
