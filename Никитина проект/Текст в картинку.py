16c72bc7458dbb6beb47474f5f4be3f2892f5353
Grigoriy <bg_1988@mail.ru>
2024-11-07 21:35:44 +1200

    9 задание

--- Никитина проект/Текст в картинку.py
+++ Никитина проект/Текст в картинку.py
@@ -1,8 +1,27 @@
+from PIL import Image
+import itertools
+
+im_input = Image.open('input.png')
+pixels = im_input.load()
+x, y = im_input.size
+
 f_input = open('input.txt', encoding='utf-8')
-f_output = open('otput.txt', mode='w')
+
+list_code = []
 for line in f_input:
     for sim in line:
         code = bin(ord(sim))[2:]
         code = '0' * (12 - len(code)) + code
+        list_code.append(code)
 
-        print(code)
+n = 0
+for i, j in itertools.product(range(x), range(y)):
+    r, g, b = pixels[i, j]
+    if n < len(list_code):
+        r = (r + int(list_code[n][0:4], 2)) % 255
+        g = (g + int(list_code[n][4:8], 2)) % 255
+        b = (b + int(list_code[n][8:],2)) % 255
+    n += 1
+    pixels[i, j] = r, g, b
+im_input.save('output.png')
+f_input.close()
---
+++ Никитина проект/Текст из картинок.py
@@ -0,0 +1,25 @@
+from PIL import Image
+import itertools
+
+
+f_output = open('output.txt', mode='w', encoding='utf-8')
+
+
+im_input = Image.open('input.png')
+im_code = Image.open('output.png')
+pixels = im_input.load()
+pixels_code = im_code.load()
+x, y = im_input.size
+for i, j in itertools.product(range(x), range(y)):
+    r, g, b = pixels[i, j]
+    r_2, g_2, b_2 = pixels_code[i, j]
+    R = r_2 - r if r_2 - r >= 0 else 255 +r_2 - r
+    G = g_2 - g if g_2 - g>= 0 else 255 + g_2 - g
+    B = b_2 - b if b_2 - b >= 0 else 255 + b_2 - b
+    # f_output.write(str(R)+str(G)+str(B) + '\n')
+    code = int(('0'* (4-len(bin(R)[2:]))+bin(R)[2:] +'0'*(4-len(bin(G)[2:]))+ bin(G)[2:] + '0'*(4-len(bin(B)[2:]))+bin(B)[2:]),2)
+    # print(code)
+    if code == 0:
+        break
+    f_output.write(str(chr(code)))
+f_output.close()
