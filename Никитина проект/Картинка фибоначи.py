a82f3360645e722241a816d1043e10c9e6b60bcd
Grigoriy <bg_1988@mail.ru>
2024-11-10 20:23:27 +1200

    9 задание

--- Никитина проект/Картинка фибоначи.py
+++ Никитина проект/Картинка фибоначи.py
@@ -2,21 +2,22 @@ from PIL import Image
 import itertools
 from  random import randint
 def point():
-    R= randint(250,255)
-    G = randint(240,255)
-    B = randint(230,255)
+    R= randint(50,255)
+    G = randint(50,255)
+    B = randint(50,255)
     return R,G,B
 
 im = Image.new("RGB", (500, 500), (0, 255, 0))
 pixels = im.load()
 x, y = im.size
 n = 0
-k = 54
+k = randint(10,100)
 r, g, b = point()
 for i, j in itertools.product(range(x), range(y)):
 
     if n == k:
         n = 0
+        k = randint(10, 100)
         r, g, b = point()
     else:
         n+=1
