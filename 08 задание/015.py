2623c818c61ca7cab9876e5a0efd502685c5b3f6
Grigoriy <bg_1988@mail.ru>
2024-11-13 01:00:13 +1200

    11 задание (55) https://amp1996.ru/perevod-ballov-ege-po-informatike

--- 08 задание/015.py
+++ 08 задание/015.py
@@ -5,7 +5,7 @@ for i in permutations('01234567',6):
     s = ''.join(i)
     if s[0]!='0':
         s1= s
-        s= s.replace('2','0').replace('4','0').replace("6","0")
+        s= s.replace('2','0').replace('4','0').replace("06","0")
         s = s.replace("3","1").replace("5","1").replace("7","1")
         if '00' not in s and '11' not in s:
             step+=1
