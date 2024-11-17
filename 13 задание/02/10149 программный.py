ad48a7cab67ba8c487f068a65519e970213c14b6
bg_19 <bg_1988@mail.ru>
2024-11-17 23:45:53 +1200

    13

--- 13 задание/02/10149 программный.py
+++ 13 задание/02/10149 программный.py
@@ -3,4 +3,3 @@ for mask in range(33):
     net = ip_network(f'220.128.112.142/{mask}',0)
     if '220.128.96.0' in str(net):
         print(net,net.netmask)
-#45 vby
---
+++ 13 задание/05/10160.py
@@ -0,0 +1,11 @@
+from ipaddress import *
+
+ip_узла = "76.155.48.2"
+ip_сети = '76.155.48.0'
+count = 0
+for mask in range(33):
+    net = ip_network(f'{ip_узла}/{mask}',0)
+    if ip_сети in str(net):
+        print(net, net.netmask,mask)
+        count +=1
+print(count)
