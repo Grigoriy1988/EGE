c3bcb481942279c794d9c984bd001a50ab10ca8f
bg_19 <bg_1988@mail.ru>
2024-12-01 22:08:09 +1200

    15

--- 15 задание/5 ЗАДАЧИ С ОТРЕЗКАМИ/Руками/(№ 6482).py
+++ 15 задание/5 ЗАДАЧИ С ОТРЕЗКАМИ/Руками/(№ 6482).py
@@ -1,6 +1,8 @@
 from itertools import combinations
 
 #(¬((x ∈ Q) → ((x ∈ P) ∨ (x ∈ R)))) → (¬(x ∈ A) → ¬(x ∈ Q))
+#(¬(Q → (P ∨ R))) → (¬A → ¬Q)
+#(( not Q and (P ∨ R))) and (A and ¬Q)
 def f(x):
     P = 1023 <= x <= 2148
     Q = 1362 <= x <= 3898
@@ -12,7 +14,7 @@ Ox  = [i  for i in range(1022,3899)]
 dx = [] # длинны отрезка а
 for a1,a2 in combinations(Ox,2):
     if all(f(x) == 1 for x in Ox):
-        # print(a2-a1)
+        print(a2-a1)
         dx.append(a2 - a1)
 print(min(dx))
 # print('\n'.join(str(i) for i in dx))
