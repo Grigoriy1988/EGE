1d81cd6bb24933857bb5b7e89c4000400db01aec
bg_19 <bg_1988@mail.ru>
2025-02-01 15:50:55 +1200

    4 задание СтатГрад

--- Варианты/04 статград/решение/8.py
+++ Варианты/04 статград/решение/8.py
@@ -8,3 +8,15 @@ while a > 0:
 
 print(len(a15), a15[::-1])  # максимальное количество разрядов и само число
 print(4 * 14 * 13 * 12 * 11 * 10 * 9 * 8) # ответ
+# Вариант перебор, очень плохой способ
+res = set()
+for i in permutations('0123456789abcde', 8):
+    if i[0] in '06789abcde':
+        continue
+    else:
+
+        dig = int(''.join(j for j in i), 15)
+        if dig <= 855_000_000:
+            # print(i)
+            res.add(dig)
+print(len(res))
