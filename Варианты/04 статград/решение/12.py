767979907db365f4fb93a0bc32d302b12bef6507
bg_19 <bg_1988@mail.ru>
2025-02-03 00:22:01 +1200

    4 задание СтатГрад

--- Варианты/04 статград/решение/12.py
+++ Варианты/04 статград/решение/12.py
@@ -1,17 +1,20 @@
 from itertools import *
-res = set()
-for i in range(1,31):
+n = 0
+for i in range(1,22):
     for d in product("12",repeat=i):
         s = ''.join(d)
-        #print(s)
+        if "111" not in s and '22' not in s and s.count('1')==5:
+            n +=1
+            print(f'{n}\t{s}\t{i} ')
 
 
-        while '111' in s or '22' in s:
-            s = s.replace('111','2',1)
-            s = s.replace('222', '1', 1)
-            s = s.replace('221', '1', 1)
-            s = s.replace('122', '1', 1)
-            s = s.replace('22', '2', 1)
-        if s.count('1')  == 5:
-            res.add(s)
-print(len(res))
+
+#         while '111' in s or '22' in s:
+#             s = s.replace('111','2',1)
+#             s = s.replace('222', '1', 1)
+#             s = s.replace('221', '1', 1)
+#             s = s.replace('122', '1', 1)
+#             s = s.replace('22', '2', 1)
+#         if s.count('1')  == 5:
+#             res.add(s)
+# print(len(res))
