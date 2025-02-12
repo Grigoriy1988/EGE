665d199cd460d1808866f03b59e922ec0730a883
Prepod-i2 <bg_1988@mail.ru>
2025-02-12 14:16:05 +1200

    5 вариант

--- Варианты/06 вариант (яндекс III)/16.py
+++ Варианты/06 вариант (яндекс III)/16.py
@@ -4,9 +4,9 @@ def f(n):
     if n == 41:
         return 41
     if n % 2 == 0:
-        return f(n-1) -n
+        return f(n-1) - n
     return n*f(n-2)
 
 for i in range(41, 9095):
     f(i)
-print(f(9094) / f(9089))
+print(f(9094) // f(9089))
