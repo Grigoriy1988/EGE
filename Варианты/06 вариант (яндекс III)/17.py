3c80098a09a9f1037019ccb0426b7da35262ab70
bg_19 <bg_1988@mail.ru>
2025-02-13 21:49:53 +1200

    4 задание СтатГрад

--- Варианты/06 вариант (яндекс III)/17.py
+++ Варианты/06 вариант (яндекс III)/17.py
@@ -1,13 +1,13 @@
 def f(k):
-    return int(k % 100 == 42 and 1000 <= abs(k) <= 9999)
+    return int(abs(k) % 100 == 42 and 1000 <= abs(k) <= 9999)
 a = [int(i) for i in open('17.txt')]
 # print(a)
 m = max([i for i in a if i % 100 == 42 and 1000 <= abs(i) <= 9999])
 # print(m)
 output = [(a[i],a[i+1],a[i+2]) for i in range(len(a)-2) if (f(a[i]),f(a[i+1]),f(a[i+2])).count(1) >= 2 and a[i]+a[i+1]+a[i+2] > m]
 print(output)
-# print(len(output),max(sum(i) for i in output))
-print(len(output))
+print(len(output),max(sum(i) for i in output))
+# print(len(output))
 
 # второй вариант понимания условия
 print()
